45f852a7f17b57b4f3685601111ae90a6bf10448
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-08-12 11:58:53 +0200

    Finish task

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const session = require('express-session');
+const passportConfig = require('./config/passport');
+const passport = require('passport');
+
 
 const app = express();
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -17,13 +25,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
